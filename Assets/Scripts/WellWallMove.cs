fb4794d5c552690b762e2e30c7fb4c1f5aaac1c4
am1r4 <aa2101346@tkh.edu.eg>
2023-11-13 04:33:29 +0200

    WellMovementCode
    - added 3D model from blender, a basic shape of the well, with well walls and floor separated
    - C# script with code applied to well walls (cylinder) that makes it move down and condition for when it should stop. this can still be adjusted further depending on the game.

---
+++ Assets/Scripts/WellWallMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.3f; // Speed at which the well walls move
+    // public float targetHeight = 2f;
+    public float minHeight = 0.0f; // minimum height the well walls can move
+
+
+    void Update()
+    {
+        // Move the well walls downward
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+        //added "space.world" because otherwise it moved to the side
+        //turned gravity off as it made it fall too fast
+
+        // add additional logic to stop the movement based on certain conditions
+        // stop the movement when a specific height is reached>
+        if (transform.position.y < minHeight)
+        {
+            moveSpeed = 0f;
+    }
+}
+}
